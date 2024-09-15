5498e2083e91abacfbc2fa5c1706ea17b6f81b7a
dat_Nguyen <dat958734@gmail.com>
2024-09-15 23:46:00 +0700

    upload

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,10 +2,10 @@
     <div class="flex flex-col min-h-screen">
         <div class="overflow-hidden relative flex items-center">
             <div id="slider" class="flex w-screen transition-all duration-500" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
-                <img src="/assets/slider/slide1.webp" alt="" class="min-w-full h-[50vh]">                
-                <img src="/assets/slider/slide2.webp" alt="" class="min-w-full h-[50vh]">                
-                <img src="/assets/slider/slide3.webp" alt="" class="min-w-full h-[50vh]">                
-                <img src="/assets/slider/slide4.jpg" alt="" class="min-w-full h-[50vh]">     
+                <img src="/assets/slider/slide1.webp" alt="" class="min-w-full h-[25vh] md:h-[50vh]">                
+                <img src="/assets/slider/slide2.webp" alt="" class="min-w-full h-[25vh] md:h-[50vh]">                
+                <img src="/assets/slider/slide3.webp" alt="" class="min-w-full h-[25vh] md:h-[50vh]">                
+                <img src="/assets/slider/slide4.jpg" alt="" class="min-w-full h-[25vh] md:h-[50vh]">     
             </div>
             <div @click="prevSlide" class="absolute left-0 cursor-pointer animate-pulse text-black"><VueIcon type="mdi" :path="mdiChevronLeft" size="70"/></div>           
             <div @click="nextSlide" class="absolute right-0 cursor-pointer animate-pulse text-black"><VueIcon type="mdi" :path="mdiChevronRight" size="70"/></div>           
