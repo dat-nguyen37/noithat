0cfbf43ade3b289bf5bb89192d6f18ab5deac3ed
dat_Nguyen <dat958734@gmail.com>
2024-09-16 19:17:56 +0700

    upload

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,6 +10,16 @@
             <div @click="prevSlide" class="absolute left-0 cursor-pointer animate-pulse text-black"><VueIcon type="mdi" :path="mdiChevronLeft" size="70"/></div>           
             <div @click="nextSlide" class="absolute right-0 cursor-pointer animate-pulse text-black"><VueIcon type="mdi" :path="mdiChevronRight" size="70"/></div>           
         </div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-5 py-2">
+            <div class="flex flex-col items-center gap-5 p-10">
+                <h1 class="uppercase text-3xl font-bold">NỘI THẤT TINH TẾ</h1>
+                <p>Với kinh nghiệm hơn 24 năm trong hoàn thiện nội thất, Nhà Xinh mang đến giải pháp toàn diện trong bao gồm thiết kế, trang trí và cung cấp nội thất trọn gói. Sở hữu đội ngũ chuyên nghiệp và hệ thống 10 cửa hàng, Nhà Xinh là lựa chọn cho không gian tinh tế và hiện đại.</p>
+                <a href="" class="border-2 p-2 border-green-500 text-xl font-bold hover:text-white hover:bg-green-500">Xem thêm</a>
+            </div>
+            <div class="">
+                <img src="/assets/image/thietke.jpg" alt="w-full h-full object-cover">
+            </div>
+        </div>
     </div>
 </template>
 
